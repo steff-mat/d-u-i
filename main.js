9eb34eed375d6bdde4f84fcd88eb654c2da3f023
Stefan Maties <tools@maties.dev>
2023-05-15 00:04:25 +0200

    .

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-import './style.css';
+import './src/css/style.css';
+import './src/css/standard.css';
+import './src/css/media.css';
+import './src/css/root.css';
+import toggleNav from './src/code/toggleNav';
+
+toggleNav();
