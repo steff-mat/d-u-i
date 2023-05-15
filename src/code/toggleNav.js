1c71951815cb67aa66e07d3d722f301325c217e6
Stefan Maties <tools@maties.dev>
2023-05-15 12:16:22 +0200

    .

--- src/code/toggleNav.js
+++ src/code/toggleNav.js
@@ -1,1 +1,14 @@
-export default function toggleNav() {}
+export default function toggleNav() {
+  const nav = document.querySelector('nav');
+  const toggleButton = document.getElementById('toggle_nav_button');
+
+  toggleButton.addEventListener('click', () => {
+    if (nav.style.top === '0px') {
+      nav.style.top = '-20rem';
+      toggleButton.innerText = 'Open';
+    } else {
+      nav.style.top = '0px';
+      toggleButton.innerText = 'Close';
+    }
+  });
+}
